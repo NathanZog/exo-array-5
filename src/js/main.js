d316d96fe54d42daa08291cfa92fade118bc4c6f
NathanZog <81653297+NathanZog@users.noreply.github.com>
2021-05-03 14:45:43 +0200

    save exo array 5

---
+++ src/js/main.js
@@ -0,0 +1,46 @@
+// exo 1
+
+let medications = "Du shit, du hash";
+let tabExo = ["Je vends",medications,420,1,true];
+console.log(tabExo);
+
+// exo 2
+
+console.log(tabExo.length);
+
+// exo 3
+
+tabExo.pop();
+console.log(tabExo);
+
+// exo 4
+
+tabExo.shift()
+console.log(tabExo)
+
+// exo 5
+
+tabExo.splice(0,0,"Hello", "Je m'appelle Nathan")
+
+// exo 6
+
+tabExo.unshift("Du Lundi au lundi","Gucci et Fendi","On a les yebi")
+
+// exo 7
+
+console.log(tabExo.reverse())
+
+// exo 8
+let objet1 = "sockettes"
+let boxFanny = [objet1,objet1]
+//alert(boxFanny)
+boxFanny.unshift("creme solaire")
+boxFanny.push("t-shirt","jean","pull")
+//alert(boxFanny)
+boxFanny.splice(0,3)
+boxFanny.push("brosse a dents")
+//alert(boxFanny.length)
+boxFanny.splice(2,4)
+//alert(boxFanny)
+boxFanny.splice(0,2)
+alert(boxFanny)
